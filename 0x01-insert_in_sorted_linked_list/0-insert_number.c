bd502b2c9bfc945ffc475053432f49247d8c4091
Danucas <danrodcastillo1994@gmail.com>
2020-12-17 22:03:36 +0000

    styling

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -12,34 +12,39 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *actual = *head;
 	listint_t *new;
 
+	if (!actual)
+	{
+		new = malloc(sizeof(listint_t));
+		if (!new)
+		{return (NULL); }
+		new->n = number;
+		new->next = NULL;
+		*head = new;
+		return (new);
+	}
 	while (actual)
 	{
 		if (
 			(actual->next
-			 && actual->next->n >= number
-			 && actual->n <= number)
-			|| (!actual->next && actual->n <= number)
-		    )
+			 && actual->next->n >= number && actual->n <= number)
+			|| (!actual->next && actual->n <= number))
 		{
 			new = malloc(sizeof(listint_t));
 			if (!new)
-			{
-			    return (NULL);
-			}
+			{return (NULL); }
 			new->n = number;
 			new->next = actual->next;
 			actual->next = new;
 			return (new);
-		} else if (actual->n > number) {
-		    new = malloc(sizeof(listint_t));
-		    if (!new)
-		    {
-			return (NULL);
-		    }
-		    new->n = number;
-		    new->next = actual;
-		    *head = new;
-		    return (new);
+		} else if (actual->n > number)
+		{
+			new = malloc(sizeof(listint_t));
+			if (!new)
+			{return (NULL); }
+			new->n = number;
+			new->next = actual;
+			*head = new;
+			return (new);
 		}
 		actual = actual->next;
 	}
